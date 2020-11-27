ec56da13c914d4cbce8945792374cbf489c31809
Luis Rosado <luismrosado@yahoo.com>
2020-11-27 16:07:34 +0000

    pagination implemented

--- lib/remote_link_pagination.rb
+++ lib/remote_link_pagination.rb
@@ -1,12 +1,12 @@
-module RemoteLinkPagination 
-  class LinkRenderer < BootstrapPagination::Rails
-    def link(text, target, attributes = {})
-      attributes['data-remote'] = true
-      super
-    end
+# module RemoteLinkPagination 
+#   class LinkRenderer < BootstrapPagination::Rails
+#     def link(text, target, attributes = {})
+#       attributes['data-remote'] = true
+#       super
+#     end
 
-    def gap
-      tag :li, tag(:span, '&hellip;', class: 'gap'), 'data-go-to-page': true
-    end
-  end
-end
+#     def gap
+#       tag :li, tag(:span, '&hellip;', class: 'gap'), 'data-go-to-page': true
+#     end
+#   end
+# end
